3f7356f1789e68bd9b945e62ea24507a389ccbaf
phukoika <tangtuphu@gmail.com>
2023-12-23 03:40:44 +0700

    authentication code

---
+++ app/src/main/java/com/example/movieapp/LoginActivity.java
@@ -0,0 +1,137 @@
+package com.example.movieapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextInputEditText editTextEmail,editTextPassword;
+    private TextInputLayout inputEmail, inputPassword;
+    private Button btnLogin;
+    private TextView tvForgotPassword, tvRegister;
+    private FirebaseAuth fAuth;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initView();
+    }
+    private void initView() {
+        fAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.editTextEmail);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvForgotPassword = findViewById(R.id.tvForgetPassword);
+        tvRegister = findViewById(R.id.tvRegister);
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+        tvForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                forgot();
+            }
+        });
+    }
+
+    private void forgot() {
+        Intent intent = new Intent(LoginActivity.this, ResetActivity.class);
+        startActivity(intent);
+    }
+
+    private void register() {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    private boolean validate(String email, String password, TextInputLayout emailInputLayout, TextInputLayout passwordInputLayout) {
+        boolean isEmailValid = isValidEmail(email);
+        boolean isPasswordValid = isValidPassword(password);
+
+        if (email.isEmpty()) {
+            showError(emailInputLayout, "Email is required");
+        } else if (!isEmailValid) {
+            showError(emailInputLayout, "Invalid email address");
+        }   else {
+            clearError(emailInputLayout);
+        }
+        if(password.isEmpty()){
+            showError(passwordInputLayout, "Password is required");
+        }
+        else if (!isPasswordValid) {
+            showError(passwordInputLayout, "Invalid password");
+        } else {
+            clearError(passwordInputLayout);
+        }
+
+        return isEmailValid && isPasswordValid;
+    }
+
+    private boolean isValidEmail(String email) {
+        return !email.isEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean isValidPassword(String password) {
+        return password.length() >= 6;
+    }
+
+    private void showError(TextInputLayout inputLayout, String errorMessage) {
+        inputLayout.setError(errorMessage);
+        inputLayout.setErrorEnabled(true);
+    }
+
+    private void clearError(TextInputLayout inputLayout) {
+        inputLayout.setError(null);
+        inputLayout.setErrorEnabled(false);
+    }
+    private void login() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        if(validate(email, password, inputEmail, inputPassword)){
+            fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "Login successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Login failed: ", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            });
+        }
+    }
+
+
+}
